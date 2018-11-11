ae19cc989d46b7d0b1f47a1c465af8ff00cb2e7b
vitormcastro <vitormcastro@outlook.com>
2018-11-11 15:29:39 -0200

    Add de dois novos metodos padronizados

--- src/Utils/Utilitys.java
+++ src/Utils/Utilitys.java
@@ -1,8 +1,14 @@
 package Utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONML;
+import org.json.JSONObject;
 
 
 public class Utilitys {
@@ -11,6 +17,24 @@ public class Utilitys {
 		response.setContentType("application/json"); 
 		response.setCharacterEncoding("utf-8");
 		response.getWriter().println(json);
+	}
+	
+	public static StringBuffer GetJsonRequest(HttpServletRequest request) throws IOException {
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		
+		BufferedReader reader = request.getReader();
+		while ((line = reader.readLine()) != null)
+		    jb.append(line);
+		return jb;
+		
+	}
+	
+	public static JSONObject CreateJsonResponseError(String message, int status) throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("Status", status);
+		json.put("Message", message);
+		return json;
 	}
 
 }
