9eec70afb20e9468c601f2ea9308449eef57da22
vitormcastro <vitormcastro@outlook.com>
2018-11-11 17:50:40 -0200

    Add os metodos de select, update, delete e insert um usuario no DB

--- src/DAO/UsuarioDao.java
+++ src/DAO/UsuarioDao.java
@@ -29,5 +29,44 @@ public class UsuarioDao {
 		}
 		return user;
 	}
+	
+	public Usuario GetUsuarioById(int idUsuario) throws SQLException {
+		PreparedStatement preparedStatement = connection
+                .prepareStatement("select idUsuario, email, isAdmin from Usuario where idUsuario = ?");
+		preparedStatement.setInt(1, idUsuario);
+		ResultSet rs = preparedStatement.executeQuery();
+		Usuario user = null;
+		while(rs.next()) {
+			user = new Usuario(rs.getInt("idUsuario"), rs.getString("nome"), rs.getString("email"), rs.getBoolean("isAdmin"));
+		}
+		return user;
+	}
+	
+	public void AddUsuario(Usuario user, String senha) throws SQLException {
+		PreparedStatement preparedStatement = connection.prepareStatement("insert into Usuario(nome, email, senha, dataCriacao,isAdmin) "
+				+ "values(?,?,?,NOW(),?)");
+		preparedStatement.setString(1, user.getNome());
+		preparedStatement.setString(2, user.getEmail());
+		preparedStatement.setString(3, senha);
+		preparedStatement.setBoolean(4, user.getIsAdmin());
+		preparedStatement.executeQuery();
+	}
+	
+	public void updateUsuario(Usuario user) throws SQLException {
+		PreparedStatement preparedStatement = connection.prepareStatement("update Usuario set nome = ?, email = ?, dataAlteracao = NOW(),"
+				+ " isAdmin = ? where idUsuario = ?");
+		preparedStatement.setString(1, user.getNome());		
+		preparedStatement.setString(2, user.getEmail());
+		preparedStatement.setBoolean(3, user.getIsAdmin());
+		preparedStatement.setInt(4, user.getId());
+		preparedStatement.executeQuery();
+		
+	}
+	
+	public void ExcluirUsuario(int idUsuario) throws SQLException {
+		PreparedStatement preparedStatement = connection.prepareStatement("delete from Usuario where idUsuario = ?");
+		preparedStatement.setInt(1, idUsuario);
+		preparedStatement.executeQuery();
+	}
 
 }
