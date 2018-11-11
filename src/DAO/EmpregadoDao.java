bbeb543b80f3038b4d4224493eb44d226b686d49
vitormcastro <vitormcastro@outlook.com>
2018-11-11 18:46:39 -0200

    Add metodo de demitir, de atualizar, verificar, pegar todos os funcionarios de uma empresa.

--- src/DAO/EmpregadoDao.java
+++ src/DAO/EmpregadoDao.java
@@ -28,6 +28,51 @@ public class EmpregadoDao {
 		return arrayId;
 	}
 	
+	public void AddUsuarioInEmpresa(int idUsuario, int idEmpresa) throws SQLException {
+		if(ThisIsUsuarioIsInThisEmpresa(idUsuario, idEmpresa)) {
+			UpdateRegistroUsuarioInEmpresa(idEmpresa, idUsuario);
+		}
+		PreparedStatement preparedStatement = connection.prepareStatement("insert into Empregado(idEmpresa, idUsuario, dataAdmissao) "
+				+ "values(?, ?,NOW())");
+		preparedStatement.setInt(1, idEmpresa);
+		preparedStatement.setInt(2, idUsuario);
+		preparedStatement.executeQuery();
+	}
 	
+	private void UpdateRegistroUsuarioInEmpresa(int idEmpresa, int idUsuario) throws SQLException {
+		PreparedStatement preparedStatement = connection.prepareStatement("update Empregado set dataAdmissao = NOW(), dataDemissao is null "
+				+ "where idEmpresa = ? and idUsuario = ?");
+		preparedStatement.setInt(1, idEmpresa);
+		preparedStatement.setInt(2, idUsuario);
+		preparedStatement.executeQuery();
+	}
+	
+	private Boolean ThisIsUsuarioIsInThisEmpresa(int idUsuario, int idEmpresa) throws SQLException {
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from Empregado where idEmpresa = ? and idUsuario = ?");
+		preparedStatement.setInt(1, idEmpresa);
+		preparedStatement.setInt(2, idUsuario);
+		ResultSet rs = preparedStatement.executeQuery();
+		return rs.getRow() >0;
+	}
+	
+	public void DemitirUsuarioDaEmpresa(int idEmpresa, int idUsuario) throws SQLException {
+		PreparedStatement preparedStatement = connection.prepareStatement("Update Empregado set DataDemissao = NOW() where idEmpresa = ?"
+				+ " and idUsuario = ?");
+		preparedStatement.setInt(1, idEmpresa);
+		preparedStatement.setInt(2, idUsuario);
+		preparedStatement.executeQuery();
+	}
+	
+	public int[] GetAllFuncionariosByEmpresa(int idEmpresa) throws SQLException {
+		PreparedStatement preparedStatement = connection.prepareStatement("select idUsuario from Empregado where idEmpresa = ?");
+		preparedStatement.setInt(1, idEmpresa);
+		ResultSet rs = preparedStatement.executeQuery();
+		int[] idArray = new int[rs.getRow()];
+		int cont = 0;
+		while(rs.next()) {
+			idArray[cont] = rs.getInt("idUsuario");
+		}
+		return idArray;
+	}
 
 }
